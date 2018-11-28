5b41e71c702e5c9269415bed56b9e1035a346959
Matheus Serpa <msserpa@blaise.inf.ufrgs.br>
2018-11-28 12:32:09 -0200

    change executing parameters

--- run/run.sh
+++ run/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #time ../ModelagemFletcher.exe TTI 248 248 248 16 12.5 12.5 12.5 0.001 0.1 | tee log.txt
-# time nvprof ../ModelagemFletcher.exe TTI 472 472 472 16 12.5 12.5 12.5 0.001 0.1 &> log.txt
-time nvprof ../ModelagemFletcher.exe TTI 216 216 216 16 12.5 12.5 12.5 0.001 0.1 &> log.txt
+time nvprof ../ModelagemFletcher.exe TTI 472 472 472 16 12.5 12.5 12.5 0.001 0.1 &> log.txt
+#time nvprof ../ModelagemFletcher.exe TTI 216 216 216 16 12.5 12.5 12.5 0.001 0.1 &> log.txt
 # time nvprof ../ModelagemFletcher.exe TTI 88 88 88 16 12.5 12.5 12.5 0.001 0.1 &> log.txt
 ./compare.sh TTI.rsf /scr1/msserpa/TTI.rsf
